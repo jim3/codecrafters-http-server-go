33d209ab0086b751f201bd2943765709cf059821
jim3 <jimmy3.xyz@gmail.com>
2024-11-24 13:43:05 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -6,22 +6,39 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+// strings.Split(" ")
+func packetParser(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Processing connection:", conn.RemoteAddr())
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	fmt.Println("Response sent to client")
+}
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
-	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		// Wait for connection
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently
+		go packetParser(conn)
 	}
 }
+
+// -------------------------------------------------------------
+// HTTP/1.1 200 OK\r\n\r\n
+// line, err := reader.ReadString()
+// fmt.Println("data := bufio.NewReader(conn): ", data)
