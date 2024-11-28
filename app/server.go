6d581e4f80be5a8fe678587c7b1f7068415b745c
jim3 <jimmy3.xyz@gmail.com>
2024-11-28 11:15:25 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -5,23 +5,55 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func packetParser(conn net.Conn) {
+func httpParser(conn net.Conn) {
 	buf := make([]byte, 1024)
-	n, err := conn.Read(buf) // conn<-Read<-buf
+	n, err := conn.Read(buf) // conn -> Read -> buf
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
+	// slice request
 	req := strings.Split(string(buf[:n]), " ")
-	if req[1] != "/" {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+
+	// request line & body
+	var protocol = "HTTP/1.1 200 OK\r\n"
+	var url = strings.TrimSpace(req[1])
+	var body = strings.Split(url, "/")
+	var resBody = strings.TrimSpace(body[len(body)-1])
+	var contentLength = len(resBody)
+
+	// headers
+	var header = map[string]string{
+		"Content-Type: ":   "text/plain\r\n",
+		"Content-Length: ": strconv.Itoa(contentLength),
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+	// routes
+	switch {
+	case req[1] == "/":
+		_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		if err != nil {
+			log.Println("Error writing response:", err)
+		}
+	case strings.Contains(req[1], "/echo"):
+		_, err := conn.Write([]byte(protocol + "Content-Type: " + header["Content-Type: "] + "Content-Length: " + header["Content-Length: "] + "\r\n\r\n" + resBody))
+		if err != nil {
+			log.Println("Error writing response:", err)
+		}
+	default:
+		_, err := conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		if err != nil {
+			log.Println("Error writing response:", err)
+		}
+	}
 }
+
+// --------------------------------------------------------------
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -38,6 +70,6 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go packetParser(conn)
+		go httpParser(conn)
 	}
 }
