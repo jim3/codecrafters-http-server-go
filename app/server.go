0510cfb6468f13b8aed82cfb07c862cfc8155e65
jim3 <jimmy3.xyz@gmail.com>
2024-11-30 13:17:12 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -11,25 +12,11 @@ import (
 
 var protocol = "HTTP/1.1 200 OK\r\n"
 
-// Define handler functions
-func handleRoot(conn net.Conn) error {
-	return writeResponse(conn, "HTTP/1.1 200 OK\r\n\r\n")
+type ParserConfig struct {
+	conn     net.Conn
+	filePath string
 }
 
-func handleEcho(conn net.Conn, header map[string]string, resBody string) error {
-	response := protocol + "Content-Type: " + header["Content-Type: "] + "Content-Length: " + header["Content-Length: "] + resBody
-	return writeResponse(conn, response)
-}
-
-// userAgentLength
-func handleUserAgent(conn net.Conn, header map[string]string, userAgentLength string) error {
-	response := protocol + "Content-Type: " + header["Content-Type: "] + "Content-Length: " + userAgentLength + header["User-Agent: "]
-	return writeResponse(conn, response)
-}
-
-// -------------------------------------------------------- //
-
-// Helper for writing response
 func writeResponse(conn net.Conn, response string) error {
 	_, err := conn.Write([]byte(response))
 	if err != nil {
@@ -38,18 +25,76 @@ func writeResponse(conn net.Conn, response string) error {
 	return nil
 }
 
+// handler functions
+func root(conn net.Conn) error {
+	return writeResponse(conn, "HTTP/1.1 200 OK\r\n\r\n")
+}
+
+func echo(conn net.Conn, header map[string]string, resBody string) error {
+	response := protocol + "Content-Type: " + header["Content-Type: "] + "Content-Length: " + header["Content-Length: "] + resBody
+	return writeResponse(conn, response)
+}
+
+func userAgent(conn net.Conn, req []string) error {
+	// extract user-agent info
+	var userString string
+	var userAgentLength string
+	if len(req) > 4 {
+		userAgentString := req[4:]
+		userString = strings.TrimSpace(userAgentString[0])
+		userAgentLength = strconv.Itoa(len(userString))
+	} else {
+		userString = ""
+		userAgentLength = "0"
+	}
+
+	// header
+	header := map[string]string{
+		"Content-Type: ": "text/plain\r\n",
+	}
+
+	// send response
+	response := protocol + "Content-Type: " + header["Content-Type: "] + "Content-Length: " + userAgentLength + "\r\n\r\n" + userString
+	return writeResponse(conn, response)
+}
+
+func fileReader(conn net.Conn, filePath string, resBody string) error {
+	var fp = filePath + resBody
+
+	// check if file exists
+	if _, err := os.Stat(fp); errors.Is(err, os.ErrNotExist) {
+		response := "HTTP/1.1 404 Not Found\r\n\r\n"
+		return writeResponse(conn, response)
+	}
+
+	f, err := os.ReadFile(fp)
+	if err != nil {
+		log.Println("Cannot read file:", err)
+		return err
+	}
+
+	var fileContentLength = strconv.Itoa(len(string(f)))
+
+	header := map[string]string{
+		"Content-Type: ": "application/octet-stream\r\n",
+	}
+
+	response := protocol + "Content-Type: " + header["Content-Type: "] + "Content-Length: " + fileContentLength + "\r\n\r\n" + string(f)
+	return writeResponse(conn, response)
+}
+
 // -------------------------------------------------------- //
 
-func httpParser(conn net.Conn) {
+func httpParser(config ParserConfig) {
 	buf := make([]byte, 1024)
-	n, err := conn.Read(buf) // conn -> Read -> buf
+	n, err := config.conn.Read(buf) // conn->Read->buf
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
+	defer config.conn.Close()
 
 	// slice up request
-	req := strings.Split(string(buf[:n]), " ")
+	req := strings.Split(string(buf[:n]), " ") // conn->Read->buf->req
 
 	// request line
 	var url = strings.TrimSpace(req[1])
@@ -57,35 +102,24 @@ func httpParser(conn net.Conn) {
 	var resBody = strings.TrimSpace(body[len(body)-1])
 	var resBodyLength = len(resBody)
 
-	// First check if there's a User-Agent header before trying to access it
-	var userString string
-	var userAgentLength string
-	if len(req) > 4 {
-		userAgentString := req[4:]
-		userString = strings.TrimSpace(userAgentString[0])
-		userAgentLength = strconv.Itoa(len(userString)) + "\r\n\r\n"
-	} else {
-		userString = ""
-		userAgentLength = "0"
-	}
-
-	// Your headers map can stay the same
+	// headers
 	var header = map[string]string{
 		"Content-Type: ":   "text/plain\r\n",
 		"Content-Length: ": strconv.Itoa(resBodyLength) + "\r\n\r\n",
-		"User-Agent: ":     userString,
 	}
 
 	// routes
 	switch {
 	case req[1] == "/":
-		err = handleRoot(conn)
+		err = root(config.conn)
 	case strings.Contains(req[1], "/echo"):
-		err = handleEcho(conn, header, resBody)
+		err = echo(config.conn, header, resBody)
 	case strings.Contains(req[1], "/user-agent"):
-		err = handleUserAgent(conn, header, userAgentLength)
+		err = userAgent(config.conn, req)
+	case strings.Contains(req[1], "/files"):
+		err = fileReader(config.conn, config.filePath, resBody)
 	default:
-		err = writeResponse(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+		err = writeResponse(config.conn, "HTTP/1.1 404 Not Found\r\n\r\n")
 	}
 
 	if err != nil {
@@ -93,7 +127,7 @@ func httpParser(conn net.Conn) {
 	}
 }
 
-// --------------------------------------------------------------
+// -------------------------------------------------------- //
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -104,12 +138,22 @@ func main() {
 	}
 	defer l.Close()
 
+	// optional file path from args for stage #AP6
+	var filePath string
+	if len(os.Args) > 2 {
+		filePath = os.Args[2]
+	}
+
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go httpParser(conn)
+		config := ParserConfig{
+			conn:     conn,
+			filePath: filePath,
+		}
+		go httpParser(config)
 	}
 }
